ad1f700734afd86bbf40a9f1eb371440de960cec
korobkad3v <korobka.d3v@gmail.com>
2025-06-14 21:23:44 +0300

    feat: added legal notice paragraphs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image";
 import DragDropArea from "../components/ui/dragNdrop/dragDropArea";
 
-import {Particles} from "../components/ui/particles";
+import { Particles } from "../components/ui/particles";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen relative"  >
-        
+      <div className="flex flex-col justify-center items-center h-screen relative">
         <div className="absolute inset-0 z-0 ">
           <Particles />
         </div>
-        
-        <main className="h-full flex flex-col items-center justify-center pointer-events-auto z-1" 
-        >
+
+        <main className="h-full flex flex-col items-center justify-center pointer-events-auto z-1">
           <DragDropArea />
+          <p className=" mt-24 text-base text-center max-w-[200px] tracking-tighter leading-tight">
+           🤖 Voidground uses AI to remove background with high precision. 
+          </p>
+          <p className=" mt-5 text-base text-center max-w-[200px] tracking-tighter leading-tight">
+            Your image is processed securely on server. 
+          </p>
         </main>
       </div>
     </>
